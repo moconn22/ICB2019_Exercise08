06afa674265261f3065efb9bb746b954ab5a6468
moconn22 <moconn22@nd.edu>
2019-11-22 08:46:45 -0500

    with answers

--- Exercise08.R
+++ Exercise08.R
@@ -1,7 +1,7 @@
 setwd("Desktop/Sophomore_Year/bc/examples/exercises/ICB2019_Exercise08/")
 scores=read.table("../ICB2019_Exercise08/UWvMSU_1-22-13.txt", header = TRUE, sep = "\t", stringsAsFactors = FALSE)
 
-
+#1
 mat <- matrix(data = NA, nrow = 51, ncol = 3)
 mat[1,]=0
 for (points in 1:nrow(scores)) {
@@ -14,22 +14,23 @@ for (points in 1:nrow(scores)) {
     mat[points+1,2]<- mat[points,2]
   }
 }
-plot(mat$)
-
+plot(x=mat[,1], y=mat[,2], type = "l", xlab = "time", ylab = "score", col="blue")
+lines(x=mat[,1], y=mat[,3], col="green")
+legend("topleft", legend=c("UW", "MSU"), col=c("blue", "green"), lwd = 1)
 #2
-GuessANumber <- function( low, high ) {
-  print( sprintf("Guess a number between 1 and 100 until you get it right", 1, 100 ) );
-  X <- 1:100;
-  number <- sample( X, 1 );
-  repeat {
-    input <- as.numeric(readline());
+GuessANumber <- function() {
+  print("Guess a number between 1 and 100 until you get it right")
+  X <- 1:100
+  number <- sample( X, 1 )
+  for (i in 1:10) {
+    input <- as.numeric(readline())
     if (input > number) {
-      print("lower"); }
+      print("lower") }
     else if (input < number) {
-      print("higher");}
+      print("higher")}
     else {
-      print("Correct!");
-      break; }
+      print("Correct!")
+      break
+      }
   }
 }
-#how to make it to where there is only 10 tries, also is this right where it is only 1-100
